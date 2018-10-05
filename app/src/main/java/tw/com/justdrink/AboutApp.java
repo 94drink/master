c314166dddd02bdc8111a59e50e29d11bea8c4fa
Jean <94drink@gmail.com>
2018-10-05 16:11:08 +0800

    AboutApp更新換圖功能

--- app/src/main/java/tw/com/justdrink/AboutApp.java
+++ app/src/main/java/tw/com/justdrink/AboutApp.java
@@ -1,5 +1,7 @@
 package tw.com.justdrink;
 
+import android.app.Service;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,11 +12,26 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import tw.com.justdrink.R;
 
+//s1005
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Vibrator;
+//1005e
+
 //Author Yi-Chun,Kuo(2018/10/4)
 public class AboutApp extends Fragment {
 
     private TextView textView1, textView2;
     private ImageView imageView1;
+
+    //s1005
+    private Vibrator vib;
+    private SensorManager sensor_manager;
+    private MySensorEventListener listener;
+    //1005e
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,8 +42,47 @@ public class AboutApp extends Fragment {
         textView1 = (TextView) rootView.findViewById(R.id.about_view1);
         textView2 = (TextView) rootView.findViewById(R.id.about_view2);
 
-        imageView1 = (ImageView) rootView.findViewById(R.id.imageView);
+        //s1005
+        //sensor_manager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        //vib = (Vibrator) getActivity().getApplication().getSystemService(Service.VIBRATOR_SERVICE);
+        sensor_manager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        vib = (Vibrator)getActivity().getSystemService(Service.VIBRATOR_SERVICE);
+        //1005e
+        imageView1 = (ImageView) rootView.findViewById(R.id.about_view3);
 
+        //s1005
+        // 接近傳感器
+        Sensor sensor = sensor_manager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        listener = new MySensorEventListener();
+        sensor_manager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        //1005e
         return rootView;
     }
+
+    //s1005
+    // 感應器事件監聽器
+    private class MySensorEventListener implements SensorEventListener {
+
+        // 感應器有了改變的回呼函示
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            final float proxyValue = event.values[0];
+
+
+                    if (proxyValue < 1) {
+                        //vib.vibrate(3000);
+                        imageView1.setImageResource(R.drawable.group_pic2);
+                    } else {
+                        //vib.vibrate(1000);
+                        imageView1.setImageResource(R.drawable.group_pic);
+                    }
+                }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+
+
+    };
 }
